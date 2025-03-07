1e04833e00d5fbb7f96b07964e8d8d29bdec1224
Joseph Poveda Flores <josephpf12@hotmail.com>
2025-03-07 11:39:40 -0600

    Smart Contract structure

--- Smart Contracts y Blockchain/Bloque 1/Primeros pasos en Solidity/conceptos_basicos.sol
+++ Smart Contracts y Blockchain/Bloque 1/Primeros pasos en Solidity/conceptos_basicos.sol
@@ -1,3 +1,7 @@
-pragma solidity ^0.4.0; //Versión única
+//pragma solidity ^0.4.0; //Versión única
 
 pragma solidity >=0.4.0 <0.7.0; //Versiones múltiples
+
+contract PrimerContrato {
+    //Estructura de un contrato
+}
