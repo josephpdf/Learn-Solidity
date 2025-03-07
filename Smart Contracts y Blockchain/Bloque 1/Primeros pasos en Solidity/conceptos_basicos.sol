c6b292bd0e5e9cdb4e72fd0d7ffce638cd7afd2a
Joseph Poveda Flores <josephpf12@hotmail.com>
2025-03-07 13:21:44 -0600

    Create .gitignore

--- Smart Contracts y Blockchain/Bloque 1/Primeros pasos en Solidity/conceptos_basicos.sol
+++ Smart Contracts y Blockchain/Bloque 1/Primeros pasos en Solidity/conceptos_basicos.sol
@@ -1,7 +1,26 @@
-//pragma solidity ^0.4.0; //Versión única
+// SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.0 <0.7.0; //Versiones múltiples
+//Inidicamos la version
+//pragma solidity ^0.4.0; //Versión unica
+pragma solidity >=0.4.0 <0.8.28; //Versiones multiples
+
+//Importar el archivo ERC20.sol que está en nuestro directorio de trabajo
+import "./ERC20.sol";
 
 contract PrimerContrato {
     //Estructura de un contrato
+    //En esta variable se encuentra la direccion de la persona que despliega el contrato
+    address owner;
+    address owner; //Variable tipo dirección - Propietario del contrato
+    ERC20Basic token;
+
+    /*
+    Guardamos en la variable owner la direccion de la persona que despliega el contrato
+    inicializamos el numero de tokens
+    */
+    constructor() public {
+        //Constructor típico
+        owner = msg.sender;
+        token = new ERC20Basic(1000);
+    }
 }
